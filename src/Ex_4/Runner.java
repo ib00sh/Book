8a3ef6ab8bfa21e456b130f5c8abcdb295b93678
max bushuev <wowmaxwow98@gmail.com>
2019-06-16 22:12:03 +0300

    Добавлен метод взимания процента при переводе свыше 100

--- src/Ex_4/Runner.java
+++ src/Ex_4/Runner.java
@@ -4,12 +4,14 @@ public class Runner {
     public static void main(String[] args) {
         Account from = new Account(78031864L, 258.5);
         Account to = new Account(58520009L, 12.1);
-        TransferAction action = new TransferAction(52.0);
+       // TransferAction action = new TransferAction(52.0);
+        TransferAction action = new TransferAction(102.0);
         boolean complete = action.tranferIntoAccount(from, to);
         if(complete){
             System.out.println("Сумма: " + action.getTransactionAmount() + " переведена успешно");
             System.out.print("На счету клиента ID= " + to.getId());
             System.out.println(" находится сумма: " + to.getAmount());
+
         }
         else{
             System.out.println("Транзакция не выполненна.");
--- src/Ex_4/TransferAction.java
+++ src/Ex_4/TransferAction.java
@@ -3,15 +3,19 @@ package Ex_4;
 public class TransferAction {
     private double transactionAmount;
     public TransferAction (double amount){
-        if (amount > 0) {
+        if (amount > 0 && amount < 100) {
             this.transactionAmount = amount;
+        }
+        else if(amount >= 100){
+            this.transactionAmount = amount - holdPercent(amount);
+            System.out.println("Взимается процент при переводе: " + holdPercent(amount));
         }
         else {
             throw new IllegalArgumentException();
         }
     }
     public boolean tranferIntoAccount(Account from, Account to) {
-        double damand = from.getAmount() - transactionAmount;
+        double damand = from.getAmount() - transactionAmount ;
         if (damand >= 0) {
             from.setAmount(damand);
             to.addAmount(transactionAmount);
@@ -22,6 +26,12 @@ public class TransferAction {
     }
     public double getTransactionAmount() {
         return transactionAmount;
+    }
+
+    public double holdPercent (double amount) {
+        double percent = amount * 0.01;
+        return percent;
+
     }
 
 
