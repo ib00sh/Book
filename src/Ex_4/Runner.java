296970ed43544cf1b8aa5358c56343eea9184689
max bushuev <wowmaxwow98@gmail.com>
2019-06-17 07:52:29 +0300

    Добавлен статический метод при переводе денежных средства

--- src/Ex_4/Runner.java
+++ src/Ex_4/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         Account from = new Account(78031864L, 258.5);
         Account to = new Account(58520009L, 12.1);
-       // TransferAction action = new TransferAction(52.0);
+      //  TransferAction action = new TransferAction(52.0);
         TransferAction action = new TransferAction(102.0);
         boolean complete = action.tranferIntoAccount(from, to);
         if(complete){
--- src/Ex_4/TransferAction.java
+++ src/Ex_4/TransferAction.java
@@ -1,20 +1,9 @@
 package Ex_4;
 
 public class TransferAction {
-    private double transactionAmount;
-    public TransferAction (double amount){
-        if (amount > 0 && amount < 100) {
-            this.transactionAmount = amount;
-        }
-        else if(amount >= 100){
-            this.transactionAmount = amount - holdPercent(amount);
-            System.out.println("Взимается процент при переводе: " + holdPercent(amount));
-        }
-        else {
-            throw new IllegalArgumentException();
-        }
-    }
-    public boolean tranferIntoAccount(Account from, Account to) {
+    private static double transactionAmount;
+    private int id;
+    public static boolean tranferIntoAccount(Account from, Account to) {
         double damand = from.getAmount() - transactionAmount ;
         if (damand >= 0) {
             from.setAmount(damand);
@@ -24,14 +13,8 @@ public class TransferAction {
             return false;
         }
     }
-    public double getTransactionAmount() {
-        return transactionAmount;
-    }
-
-    public double holdPercent (double amount) {
-        double percent = amount * 0.01;
-        return percent;
-
+    public void increaseAmount(){
+        transactionAmount++;
     }
 
 
